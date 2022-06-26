16ce1a6d9755fec4af83855741e19da2db0c9264
Marcilio-Oliveira-Vale <89142492+Marcilio-Oliveira-Vale@users.noreply.github.com>
2022-06-26 19:58:34 -0300

    firebase

--- js/professores.js
+++ js/professores.js
@@ -1,4 +1,4 @@
-var dataRef = new Firebase('https://escola-d5b2f-default-rtdb.firebaseio.com/');
+var dataRef = new Firebase('https://siteclinica-20ca1-default-rtdb.firebaseio.com/');
 
 dataRef.on('value', function(snapshot) {
 
@@ -11,35 +11,20 @@ dataRef.on('value', function(snapshot) {
         console.log('chave', key);
         console.log('valor', valor);
 
-        if(key === 'professores') {
+        if(key === 'medicos') {
 
             var _itemsKey = Object.entries(valor);
     
             count++;
-    
-    
+
             for (const [key2, value2] of _itemsKey) {
-    
-                $("#tbProfessores tbody").append(
-    
-                    `<tr> +
-    
-    
-                        <td>${value2.nome}</td> +
-
-                        <td>${value2.telefone}</td> +
-
-                        <td>${value2.email}</td> +
-        
-                        <td>${value2.formação}</td> +
-                        
-
-    
-                    </tr>`
-    
-                );
-    
+                console.log(key2, value2);
             }
+    
+    
+                
+    
+        
         }
 
 
