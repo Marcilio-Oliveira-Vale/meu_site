a158e685f6ca26652ee820e658a6f11baa2dbf3f
Marcilio-Oliveira-Vale <89142492+Marcilio-Oliveira-Vale@users.noreply.github.com>
2022-06-26 19:12:15 -0300

    firebase

--- js/firebase.js
+++ js/firebase.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		for (var key in t) {
 
 			console.log(key);
-			if(key === 'eventos') {
+			if(key === 'medicos') {
 
 				// document.querySelector('#bloco-eventos-article').html( '' );
 
@@ -19,31 +19,29 @@ $(document).ready(function() {
 	
 					for (const [key2, value2] of _itemsKey) {
 						console.log(key2, value2);
+                        
 
-						if(value2.isAtivo === true) {
-
-							console.log(value2.desc)
-							console.log(value2.link)
-
-							var newId = 'medicos'+count;
-							var clone = document.querySelector('#medicos-clone').cloneNode( true );
-							clone.setAttribute('id', newId);
+						var newId = 'medicos'+count;
+						var clone = document.querySelector('#medicos-clone').cloneNode( true );
+						clone.setAttribute('id', newId);
 							
-							var img_url = clone.getElementsByClassName("img_url")[0];
-							img_url.setAttribute("src", value2.img_url);
+						var img_url = clone.getElementsByClassName("img_url")[0];
+						img_url.setAttribute("src", value2.img_url);
 
-							var nome = clone.getElementsByClassName("nome")[0];
-							nome.textContent = value2.nome;
+						var nome = clone.getElementsByClassName("nome")[0];
+						nome.textContent = value2.nome;
 
-							var especialidade = clone.getElementsByClassName("especialidade")[0];
-							especialidade.textContent = value2.desc;
+						var especialidade = clone.getElementsByClassName("especialidade")[0];
+						especialidade.textContent = value2.desc;
 
 	
 
 
-							document.querySelector('#medicos-clone').appendChild( clone );
+						document.querySelector('#medicos-clone').appendChild( clone );
 
-						}
+							
+
+					
 
 					}	  
 				}
